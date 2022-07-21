dc75ed4ac38bfcb4fa1530d1e48a69569b92f986
Loromi <loromi94@gmail.com>
2022-07-21 15:37:49 +0200

    feat: created main package and class

---
+++ src/main/GuessTheMovie.java
@@ -0,0 +1,10 @@
+package main;
+
+public class GuessTheMovie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to guess the movie. Game still needs to be built...");
+	}
+
+}
