670780dd2587fc4cd34459cb0d369958d2cf4918
Loromi <loromi94@gmail.com>
2022-07-21 17:11:01 +0200

    feat: iterate trough lines of movies.txt and return random index as string

--- src/main/GuessTheMovie.java
+++ src/main/GuessTheMovie.java
@@ -1,10 +1,57 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class GuessTheMovie {
+	
+	public static String getMovie() {
+		
+		String pathname = System.getProperty("user.dir")+"/movies.txt";
+		File movies = new File(pathname);
+		System.out.println("File exists: " + movies.exists());
+		
+		try {
+			Scanner scanner = new Scanner(movies, StandardCharsets.UTF_8);
+			
+			int i = 0;
+			while (scanner.hasNextLine()) {
+				i++;
+				scanner.nextLine();
+			} 
+			
+			String[] lines = new String[i+1];
+			scanner = new Scanner(movies, StandardCharsets.UTF_8);
+			
+			int j = 0;
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				lines[j] = line;
+				j++;
+			}
+			
+			System.out.println("Number of movies: " + lines.length);
+			int k = ThreadLocalRandom.current().nextInt(0, lines.length);
+			String movie = lines[k];
+			System.out.println("random movie: " + movie);
+			
+			return movie;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "IOException";
+		}
+		
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to guess the movie. Game still needs to be built...");
+		String movie = getMovie();
 	}
 
 }
